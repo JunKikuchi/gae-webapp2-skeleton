2a1f6098498332e956e93b5e33837928be0d4648
Jun Kikuchi <kikuchi@bonnou.com>
2012-03-20 16:58:00 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import webapp2
+from webapp2_extras import jinja2
+
+def jinja2_factory(app):
+  j = jinja2.Jinja2(app)
+  j.environment.globals.update({
+    'uri_for': webapp2.uri_for,
+  })
+  return j
+
+class BaseHandler(webapp2.RequestHandler):
+  @webapp2.cached_property
+  def jinja2(self):
+    return jinja2.get_jinja2(factory=jinja2_factory)
+
+  def render_response(self, template, **context):
+    self.response.write(self.jinja2.render_template(template, **context))
+
+class IndexPage(BaseHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/html'
+    self.render_response('index.html')
+
+app = webapp2.WSGIApplication([
+  webapp2.Route(r'/', handler=IndexPage, name='index'),
+])
