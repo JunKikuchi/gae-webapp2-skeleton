9514202ee47b7187b8dd25778ce8bc8da52bd2ec
Jun Kikuchi <kikuchi@bonnou.com>
2012-05-03 11:04:44 +0900

    セッションハンドラーを読みやすく改造した

--- webapp2_local/session.py
+++ webapp2_local/session.py
@@ -3,11 +3,14 @@ from webapp2_extras import sessions
 
 class SessionHandler(webapp2.RequestHandler):
   def dispatch(self):
-    self.session_store = sessions.get_store(request=self.request)
     try:
       webapp2.RequestHandler.dispatch(self)
     finally:
       self.session_store.save_sessions(self.response)
+
+  @webapp2.cached_property
+  def session_store(self):
+    return sessions.get_store(request=self.request)
 
   @webapp2.cached_property
   def session(self):
